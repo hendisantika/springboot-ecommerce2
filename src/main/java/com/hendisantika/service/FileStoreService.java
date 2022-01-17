6a5986eae17d07f021f347f82998d282f7e3d992
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:49:20 +0700

    #54 - Add load in FileStoreService

--- src/main/java/com/hendisantika/service/FileStoreService.java
+++ src/main/java/com/hendisantika/service/FileStoreService.java
@@ -3,12 +3,15 @@ package com.hendisantika.service;
 import com.hendisantika.config.StorageProperties;
 import com.hendisantika.exception.StorageException;
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -66,6 +69,20 @@ public class FileStoreService {
         } catch (IOException e) {
             throw new StorageException("Failed to read stored files", e);
         }
+    }
 
+    public Resource load(String filename) {
+        try {
+            Path file = rootLocation.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
     }
 }
