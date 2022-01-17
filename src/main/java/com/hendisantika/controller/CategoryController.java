eac887d01fcb69dfa0fbc58553603d70b3014227
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:08:25 +0700

    #75 - Add getCategories in CategoryController

--- src/main/java/com/hendisantika/controller/CategoryController.java
+++ src/main/java/com/hendisantika/controller/CategoryController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Category;
 import com.hendisantika.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +26,10 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Category>> getCategories() {
+        List<Category> body = categoryService.listCategories();
+        return new ResponseEntity<List<Category>>(body, HttpStatus.OK);
+    }
 }
