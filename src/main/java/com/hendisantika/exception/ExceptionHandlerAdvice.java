a06460ab249d9373bb81df62423e5f12ac83db07
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:35:55 +0700

    #38 - Add StorageException

--- src/main/java/com/hendisantika/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/hendisantika/exception/ExceptionHandlerAdvice.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import javax.validation.ValidationException;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-ecommerce2
---
+++ src/main/java/com/hendisantika/exception/StorageException.java
@@ -0,0 +1,21 @@
+package com.hendisantika.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.35
+ */
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
