fcb1d2f0d776d2d209af93ecaaeb2b997eef1613
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:12:31 +0700

    #81 - Add getFile in FileUploadController

--- src/main/java/com/hendisantika/controller/FileUploadController.java
+++ src/main/java/com/hendisantika/controller/FileUploadController.java
@@ -3,9 +3,12 @@ package com.hendisantika.controller;
 import com.hendisantika.entity.FileInfo;
 import com.hendisantika.service.FileStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,5 +54,12 @@ public class FileUploadController {
 
         Stream<Path> pathStream = fileStoreService.loadAll();
         return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = fileStoreService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 }
