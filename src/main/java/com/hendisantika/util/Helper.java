3f61c79de56028f3370cab0c8153dc1a3847a245
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:20:57 +0700

    #28 - Add Helper

---
+++ src/main/java/com/hendisantika/util/Helper.java
@@ -0,0 +1,16 @@
+package com.hendisantika.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.20
+ */
+public class Helper {
+    public static boolean notNull(Object obj) {
+        return obj != null;
+    }
+}
