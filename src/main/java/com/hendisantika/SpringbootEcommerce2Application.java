f34f176b8c7693b8119666ccdc6dde5a66e1ac9c
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:23:55 +0700

    #96 - Add SpringBootServletInitializer in Main App

--- src/main/java/com/hendisantika/SpringbootEcommerce2Application.java
+++ src/main/java/com/hendisantika/SpringbootEcommerce2Application.java
@@ -2,12 +2,18 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootEcommerce2Application {
+public class SpringbootEcommerce2Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootEcommerce2Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootEcommerce2Application.class);
+    }
 }
