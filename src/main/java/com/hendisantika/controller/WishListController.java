77a79e0cb5414cc1aa47c8f4ff4e23fbd99e26b4
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:20:48 +0700

    #93 - Add WishListController

---
+++ src/main/java/com/hendisantika/controller/WishListController.java
@@ -0,0 +1,28 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.AuthenticationService;
+import com.hendisantika.service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/01/22
+ * Time: 06.20
+ */
+@RestController
+@RequestMapping("/wishlist")
+public class WishListController {
+
+    @Autowired
+    private WishListService wishListService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+}
