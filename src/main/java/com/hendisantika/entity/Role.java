93d345481be87fe2fa8af53bc68a6caba028fb06
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:02:31 +0700

    #12 - Add Role

---
+++ src/main/java/com/hendisantika/entity/Role.java
@@ -0,0 +1,16 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.02
+ */
+public enum Role {
+    user,
+    manager,
+    admin
+}
