56b886f4d61a003489b30c509fda026b6527a11a
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:19:24 +0700

    #91 - Add getUsers in UserProfileController

--- src/main/java/com/hendisantika/controller/UserProfileController.java
+++ src/main/java/com/hendisantika/controller/UserProfileController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.UserProfile;
 import com.hendisantika.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +26,10 @@ public class UserProfileController {
 
     @Autowired
     private UserProfileService userProfileService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserProfile>> getUsers() {
+        List<UserProfile> dtos = userProfileService.listProfiles();
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
 }
