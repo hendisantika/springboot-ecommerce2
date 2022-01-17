2aab7d2c4c3aa88cbb3c7d5f50146c5c2b8b7705
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:02:08 +0700

    #11 - Add ResponseStatus

---
+++ src/main/java/com/hendisantika/entity/AuthenticationToken.java
@@ -0,0 +1,13 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.00
+ */
+public class AuthenticationToken {
+}
---
+++ src/main/java/com/hendisantika/entity/ResponseStatus.java
@@ -0,0 +1,15 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.01
+ */
+public enum ResponseStatus {
+    success,
+    error
+}
