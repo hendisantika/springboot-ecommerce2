f0ffa84906b3897c0e291888049d56a573277e4b
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:39:11 +0700

    #41 - Add MessageStrings Configs

---
+++ src/main/java/com/hendisantika/config/MessageStrings.java
@@ -0,0 +1,20 @@
+package com.hendisantika.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.38
+ */
+public class MessageStrings {
+
+    public static final String USER_NOT_PERMITTED = "user is not permitted to perform this operation";
+    public static final String AUTH_TOKEN_NOT_PRESENT = "authentication token not present";
+    public static final String AUTH_TOKEN_NOT_VALID = "authentication token not valid";
+    public static final String USER_CREATED = "user created successfully";
+    public static final String ID_NOT_PRESENT = "primary key is required for updating";
+    public static final String WRONG_PASSWORD = "please check the pass";
+}
