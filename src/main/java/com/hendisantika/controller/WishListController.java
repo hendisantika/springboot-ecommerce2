92dba6ac852a0b57319a048234255fc04aa4a3c4
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:21:55 +0700

    #94 - Add getWishList in WishListController

--- src/main/java/com/hendisantika/controller/WishListController.java
+++ src/main/java/com/hendisantika/controller/WishListController.java
@@ -1,10 +1,20 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.ProductDto;
+import com.hendisantika.entity.WishList;
 import com.hendisantika.service.AuthenticationService;
+import com.hendisantika.service.ProductService;
 import com.hendisantika.service.WishListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +35,14 @@ public class WishListController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<ProductDto>> getWishList(@PathVariable("userId") Integer userId) {
+        List<WishList> body = wishListService.readWishList(userId);
+        List<ProductDto> products = new ArrayList<>();
+        for (WishList wishList : body) {
+            products.add(ProductService.getDtoFromProduct(wishList.getProduct()));
+        }
+
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
