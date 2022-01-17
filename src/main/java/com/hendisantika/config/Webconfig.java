bc18530c2f59c87839ff5f137ab5eecad6cf83c1
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:40:18 +0700

    #43 - Add Webconfig

---
+++ src/main/java/com/hendisantika/config/Webconfig.java
@@ -0,0 +1,31 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.40
+ */
+@Configuration
+class Webconfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
