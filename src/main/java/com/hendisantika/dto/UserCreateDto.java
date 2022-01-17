0f853b58a4b96ff920de76a3e502297e85530ca2
Hendi Santika <hendisantika@gmail.com>
2022-01-17 07:59:24 +0700

    #9 - Add UserCreateDto

---
+++ src/main/java/com/hendisantika/dto/UserCreateDto.java
@@ -0,0 +1,21 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 07.59
+ */
+@Data
+public class UserCreateDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
+    private String password;
+}
