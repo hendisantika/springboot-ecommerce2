d73e943ae77116419a3f2615f0043a198f8a47ae
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:06:49 +0700

    #16 - Add Category

---
+++ src/main/java/com/hendisantika/entity/Category.java
@@ -0,0 +1,48 @@
+package com.hendisantika.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.06
+ */
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+public class Category {
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    Set<Product> products;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "category_name")
+    private @NotBlank String categoryName;
+    private @NotBlank String description;
+
+    // add imageURL here
+    private @NotBlank String imageUrl;
+
+    public Category(@NotBlank String categoryName, @NotBlank String description) {
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
+    public Category(@NotBlank String categoryName, @NotBlank String description, @NotBlank String imageUrl) {
+        this.categoryName = categoryName;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+}
