bde8b34589fffa97ec9d1ea5ec95e02e5a4ee9bf
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:27:26 +0700

    #29 - Add AuthenticationService

---
+++ src/main/java/com/hendisantika/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.TokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 08.27
+ */
+@Service
+public class AuthenticationService {
+
+    @Autowired
+    private TokenRepository repository;
+}
