5e98cf4b47cf53e7a8c80c31cdbb91d0a14740db
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:13:48 +0700

    #83 - Add getProducts in ProductController

--- src/main/java/com/hendisantika/controller/ProductController.java
+++ src/main/java/com/hendisantika/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.ProductDto;
 import com.hendisantika.service.CategoryService;
 import com.hendisantika.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +29,10 @@ public class ProductController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<ProductDto>> getProducts() {
+        List<ProductDto> body = productService.listProducts();
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
 }
