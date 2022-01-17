b625889c653765b28af3ea6f118be49e1310c13e
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:16:39 +0700

    #86 - Add UserController

---
+++ src/main/java/com/hendisantika/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.repository.UserRepository;
+import com.hendisantika.service.AuthenticationService;
+import com.hendisantika.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/01/22
+ * Time: 06.16
+ */
+@RequestMapping("user")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private UserService userService;
+}
