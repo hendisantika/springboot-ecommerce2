66c33bdfa15b8c15ca19fe7cf436faa1560cd40d
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:15:07 +0700

    #85 - Add updateProduct in ProductController

--- src/main/java/com/hendisantika/controller/ProductController.java
+++ src/main/java/com/hendisantika/controller/ProductController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,5 +52,17 @@ public class ProductController {
         Category category = optionalCategory.get();
         productService.addProduct(productDto, category);
         return new ResponseEntity<>(new ApiResponse(true, "Product has been added"), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/update/{productID}")
+    public ResponseEntity<ApiResponse> updateProduct(@PathVariable("productID") long productID,
+                                                     @RequestBody @Valid ProductDto productDto) {
+        Optional<Category> optionalCategory = categoryService.readCategory(productDto.getCategoryId());
+        if (!optionalCategory.isPresent()) {
+            return new ResponseEntity<>(new ApiResponse(false, "category is invalid"), HttpStatus.CONFLICT);
+        }
+        Category category = optionalCategory.get();
+        productService.updateProduct(productID, productDto, category);
+        return new ResponseEntity<>(new ApiResponse(true, "Product has been updated"), HttpStatus.OK);
     }
 }
