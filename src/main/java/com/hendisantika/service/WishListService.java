628a1f0411141bc339a6ebe7c4860bcb99542076
Hendi Santika <hendisantika@gmail.com>
2022-01-18 06:06:17 +0700

    #71 - Add WishListService

---
+++ src/main/java/com/hendisantika/service/WishListService.java
@@ -0,0 +1,25 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.WishListRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/01/22
+ * Time: 06.05
+ */
+@Service
+@Transactional
+public class WishListService {
+
+    private final WishListRepository wishListRepository;
+
+    public WishListService(WishListRepository wishListRepository) {
+        this.wishListRepository = wishListRepository;
+    }
+}
