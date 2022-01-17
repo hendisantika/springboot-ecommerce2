fba3a4f69fac1ed8c324ae05622debd4b0c95ae0
Hendi Santika <hendisantika@gmail.com>
2022-01-17 07:59:50 +0700

    #10 - Add UserUpdateDto

---
+++ src/main/java/com/hendisantika/dto/UserUpdateDto.java
@@ -0,0 +1,20 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 07.59
+ */
+@Data
+public class UserUpdateDto {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Role role;
+}
