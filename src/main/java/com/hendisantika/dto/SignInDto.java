85a1ba68fa9ed9d65812b3c524066fe8c6857c27
Hendi Santika <hendisantika@gmail.com>
2022-01-17 07:57:48 +0700

    #6 - Add SignInDto

---
+++ src/main/java/com/hendisantika/dto/SignInDto.java
@@ -0,0 +1,18 @@
+package com.hendisantika.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/22
+ * Time: 07.57
+ */
+@Data
+public class SignInDto {
+    private String email;
+    private String password;
+}
