0cf692f6dc3894737c15235e7f59d74cca03eecd
Hendi Santika <hendisantika@gmail.com>
2022-01-17 07:48:42 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringbootEcommerce2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommerce2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEcommerce2Application.class, args);
+    }
+
+}
