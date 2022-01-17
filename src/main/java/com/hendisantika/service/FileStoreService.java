c3ced812219b2730aff6a6dada2981e88a002a58
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:48:45 +0700

    #52 - Add store in FileStoreService

--- src/main/java/com/hendisantika/service/FileStoreService.java
+++ src/main/java/com/hendisantika/service/FileStoreService.java
@@ -1,10 +1,19 @@
 package com.hendisantika.service;
 
 import com.hendisantika.config.StorageProperties;
+import com.hendisantika.exception.StorageException;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +30,30 @@ public class FileStoreService {
     private final StorageProperties properties = new StorageProperties();
     private final Path rootLocation = Paths.get(properties.getLocation());
 
+    public String store(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file.");
+            }
+
+            String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+            String uploadedFileName = UUID.randomUUID() + "." + extension;
+
+            Path destinationFile = rootLocation.resolve(
+                            Paths.get(uploadedFileName))
+                    .normalize().toAbsolutePath();
+
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile,
+                        StandardCopyOption.REPLACE_EXISTING);
+
+                final String baseUrl =
+                        ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+
+                return baseUrl + "/fileUpload/files/" + uploadedFileName;
+            }
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file.", e);
+        }
+    }
 }
