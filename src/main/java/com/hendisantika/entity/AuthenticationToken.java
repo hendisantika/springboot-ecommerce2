c7ce17bd6d918d245f142f0ae07bebf9cefc01bc
Hendi Santika <hendisantika@gmail.com>
2022-01-17 08:05:52 +0700

    #15 - Add AuthenticationToken

--- src/main/java/com/hendisantika/entity/AuthenticationToken.java
+++ src/main/java/com/hendisantika/entity/AuthenticationToken.java
@@ -1,5 +1,13 @@
 package com.hendisantika.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-ecommerce2
@@ -9,5 +17,33 @@ package com.hendisantika.entity;
  * Date: 17/01/22
  * Time: 08.00
  */
+
+@Entity
+@Table(name = "tokens")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+
+    private String token;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public AuthenticationToken(User user) {
+        this.user = user;
+        this.createdDate = new Date();
+        this.token = UUID.randomUUID().toString();
+    }
+
 }
